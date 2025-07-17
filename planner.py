ba270f8260efc8ebbe07d1a283e666f540eff142
AlexVasquez01 <76964352+AlexVasquez01@users.noreply.github.com>
2025-07-17 15:20:12 -0700

    get_neighbors func, dfs logic node tracking

--- planner.py
+++ planner.py
@@ -1,3 +1,6 @@
+import sys
+import heapq
+
 # N, S, E, W
 MOVES = {
     'N': (-1, 0),
@@ -42,3 +45,42 @@ def read_world(filename):
                 dirty.add((r, c))
 
     return grid, start, dirty
+
+def get_neighbors(state, grid):
+    neighbors = []
+    r, c = state.pos
+    rows, cols = len(grid), len(grid[0])
+
+    # vacuum curr cell if dirty
+    if (r, c) in state.dirty:
+        new_dirt = set(state.dirty)
+        new_dirt.remove((r, c))
+        neighbors.append(VacuumState((r, c), new_dirt, state.path + ['V'], state.cost + 1))
+
+    # try to move in each dir
+    for action, (dr, dc) in MOVES.items():
+        nr, nc = r + dr, c + dc
+        if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] != '#':
+            neighbors.append(VacuumState((nr, nc), state.dirty, state.path + [action], state.cost + 1))
+
+    return neighbors
+
+def depth_first_search(start_state, grid):
+    stack = [start_state]
+    visited = set()
+    nodes_generated = 0
+    nodes_expanded = 0
+
+    while stack:
+        state = stack.pop()
+        if state in visited:
+            continue
+        visited.add(state)
+        nodes_expanded += 1
+        if state.is_goal():
+            return state.path, nodes_generated, nodes_expanded
+        neighbors = get_neighbors(state, grid)
+        nodes_generated += len(neighbors)
+        stack.extend(neighbors)
+
+    return None, nodes_generated, nodes_expanded
